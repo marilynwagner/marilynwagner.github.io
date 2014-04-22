92bac712e6647e978d75a666eb8158391e28537e
marilynwagner <marilynlwagner@msn.com>
2014-04-21 19:15:03 -0700

    added rest of portfolio files

---
+++ scripts/assign3.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('.tab-section').hide();
+	$('#tabs a').bind('click', function(e) {
+		$('#tabs a.current').removeClass('current');
+		$('.tab-section:visible').hide();
+	$(this.hash).show();
+		$(this).addClass('current');
+		e.preventDefault;		
+		}).filter(':first').click();
+});
