bbc43803accfaae32fa5f6a40e8d8e060ae8e0b9
marilynwagner <marilynwagner@users.noreply.github.com>
2017-02-21 16:19:39 -0800

    file not needed

--- Angular_Portfolio/myCtrl.js
+++
@@ -1,9 +0,0 @@
-app.controller("myCtrl", function($scope) {
-    $scope.result = "";
-
-  
-   $scope.clear = function() {$scope.result = "";};
-    $scope.save  = function() {
-    	alert("Saved")
-    ;};
-}); 
